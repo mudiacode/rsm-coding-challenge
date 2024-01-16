2fb270ed85c15b43540b7d85bd1cfe804200778b
mudia <o.osagie1@uni.brighton.ac.uk>
2024-01-16 11:50:43 +0000

    cleaned up code

--- coding-project/src/UniqueIdentifier.java
+++ coding-project/src/UniqueIdentifier.java
@@ -1,12 +1,8 @@
-// java.util package to create random numbers
 import java.util.Random;
 
-// Class for unique identifier used for officeNumber
 public class UniqueIdentifier{
-    // create random number with seed so number stays the same if program is reset
     private static final Random random = new Random(1);
 
-   // random number from 1 to 500
     public static Integer generateUniqueIdentifier() {
         return random.nextInt(500) + 1;
     }
